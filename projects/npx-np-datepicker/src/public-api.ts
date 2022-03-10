2779671a982900eac1629679718fd5d4f3adada5
wwwbijay <link2bj@gmail.com>
2022-03-10 15:08:36 +0545

    site branch

--- projects/npx-np-datepicker/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of npx-np-datepicker
- */
-
-export * from './lib/np-datepicker.service';
-export * from './lib/np-datepicker.component';
-export * from './lib/np-datepicker.module';
